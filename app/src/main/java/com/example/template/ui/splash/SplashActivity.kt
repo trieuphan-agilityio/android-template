79980f9a93e3cd294ec7d4ce08b9dd562fbc85b4
Trieu Phan <trieu.phanvan@asnet.com.vn>
2020-03-18 14:29:25 +0700

    Added splash screen

--- app/src/main/java/com/example/template/ui/splash/SplashActivity.kt
+++ app/src/main/java/com/example/template/ui/splash/SplashActivity.kt
@@ -4,6 +4,5 @@ import com.example.template.R
 import com.example.template.ui.common.BaseActivity
 
 class SplashActivity : BaseActivity<SplashViewModel>(SplashViewModel::class) {
-
     override fun getLayoutRes(): Int = R.layout.activity_splash
 }
